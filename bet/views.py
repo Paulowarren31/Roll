bdf8aa1deec0f98b8b9c0da82740c98888e3437c
Warren <paulowar@UMICH.EDU>
2016-08-01 16:58:29 -0400

    working slightly better. Still broken

--- bet/views.py
+++ bet/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from bet.models import Bet
 
-# Create your views here.
+def bets(request):
+  if request.user.is_authenticated():
+    args = {}
+    args['bets'] = Bet.objects.all()
+    return render(request, 'bets.html', args)
+  else:
+    return redirect('/')
+
+def bet_detail(request, id_in):
+  args = {}
+  args['bet'] = Bet.objects.filter(id=id_in)[0]
+  return render(request, 'bet.html', args)
+
